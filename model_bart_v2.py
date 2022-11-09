b5fd31e7e523752c052c233e8ed55594b087471c
qy2000 <49312264+qy2000@users.noreply.github.com>
2022-11-09 16:22:33 +0800

    clean up

--- model_bart_v2.py
+++ model_bart_v2.py
@@ -1,5 +1,4 @@
 
-
 import torch
 import torch.nn as nn
 import torch.nn.utils.rnn as R
@@ -9,117 +8,42 @@ from transformers import BartModel
 import numpy as np
 
 
-
 class PointerNetworks(nn.Module):
-    def __init__(self,voca_size, voc_embeddings,word_dim, hidden_dim,is_bi_encoder_rnn,encoder_type,decoder_type,rnn_layers,
-                 dropout_prob,use_cuda,finedtuning,isbanor):
-        super(PointerNetworks,self).__init__()
+    def __init__(self, encoder_type, decoder_type, rnn_layers, dropout_prob ,use_cuda):
+        super(PointerNetworks ,self).__init__()
 
-        self.word_dim = word_dim
-        self.voca_size = voca_size
-
-        self.hidden_dim = hidden_dim
         self.dropout_prob = dropout_prob
-        self.is_bi_encoder_rnn = is_bi_encoder_rnn
         self.num_rnn_layers = rnn_layers
         self.encoder_type = encoder_type
         self.decoder_type = decoder_type
-        self.voc_embeddings = voc_embeddings
-        self.finedtuning = finedtuning
 
         self.nnDropout = nn.Dropout(dropout_prob)
 
-        self.isbanor = isbanor
         if encoder_type == 'BART':
-
             self.encoder_bart = BartModel.from_pretrained("facebook/bart-base")
             self.hidden_dim = self.encoder_bart.config.hidden_size
 
-
         if decoder_type in ['LSTM', 'GRU']:
-
             self.decoder_rnn = getattr(nn, decoder_type)(input_size=self.hidden_dim,
                                                      hidden_size=self.hidden_dim,
                                                      num_layers=rnn_layers,
                                                      dropout=dropout_prob,
                                                      batch_first=True)
 
-
-
         else:
             print('rnn_type should be LSTM,GRU')
 
-
-
         self.nnSELU = nn.SELU()
 
-
-        self.nnEm = nn.Embedding(self.voca_size,self.word_dim)
-
-        self.initEmbeddings(self.voc_embeddings)
-
         self.use_cuda = use_cuda
-
-
-
-
-
-
-        if self.is_bi_encoder_rnn:
-            self.num_encoder_bi = 2
-        else:
-            self.num_encoder_bi = 1
-
 
         self.nnW1 = nn.Linear(self.hidden_dim, self.hidden_dim, bias=False)
         self.nnW2 = nn.Linear(self.hidden_dim, self.hidden_dim, bias=False)
         self.nnV = nn.Linear(self.hidden_dim, 1, bias=False)
 
 
-
-
-
-
-
-
-
-
-
-    def initEmbeddings(self,weights):
-        self.nnEm.weight.data.copy_(torch.from_numpy(weights))
-        self.nnEm.weight.requires_grad = self.finedtuning
-
-
-    def initHidden(self,hsize,batchsize):
-
-
-        if self.rnn_type == 'LSTM':
-
-            h_0 = Variable(torch.zeros(self.num_encoder_bi*self.num_rnn_layers, batchsize, hsize))
-            c_0 = Variable(torch.zeros(self.num_encoder_bi*self.num_rnn_layers, batchsize, hsize))
-
-            if self.use_cuda:
-                h_0 = h_0.cuda()
-                c_0 = c_0.cuda()
-
-            return (h_0, c_0)
-        else:
-
-            h_0 = Variable(torch.zeros(self.num_encoder_bi*self.num_rnn_layers, batchsize, hsize))
-
-            if self.use_cuda:
-                h_0 = h_0.cuda()
-
-
-            return h_0
-
-
-
-
-
-
-
     def _run_rnn_packed(self, cell, x, x_lens, h=None):
+
         x_packed = R.pack_padded_sequence(x, x_lens.data.tolist(),
                                           batch_first=True)
 
@@ -133,15 +57,13 @@ class PointerNetworks(nn.Module):
         return output, h
 
 
-
-
-
-    def pointerEncoder(self,Xin, Xin_mask, lens):
+    def pointerEncoder(self ,Xin, Xin_mask):
 
         model = BartModel.from_pretrained("facebook/bart-base", output_hidden_states=True)
-        print(Xin.shape, Xin_mask.shape)
         outputs = model(input_ids=Xin, attention_mask=Xin_mask)
+
         h = outputs.encoder_last_hidden_state
+
         o = outputs.encoder_hidden_states
         o = torch.stack(list(o), dim=0)
         o = o.squeeze(1)
@@ -151,56 +73,44 @@ class PointerNetworks(nn.Module):
         return o, h
 
 
-    def pointerLayer(self,en,di):
+    def pointerLayer(self, en ,di):
         """
-
         :param en:  [L,H]
         :param di:  [H,]
         :return:
         """
 
-
         WE = self.nnW1(en)
-
 
         exdi = di.expand_as(en)
 
         WD = self.nnW2(exdi)
 
-        nnV = self.nnV(self.nnSELU(WE+WD))
+        nnV = self.nnV(self.nnSELU(WE +WD))
 
-        nnV = nnV.permute(1,0)
+        nnV = nnV.permute(1 ,0)
 
         nnV = self.nnSELU(nnV)
 
-
-        #TODO: for log loss
+        # TODO: for log loss
         att_weights = F.softmax(nnV, dim=1)
         logits = F.log_softmax(nnV, dim=1)
 
+        return logits, att_weights
 
 
-
-        return logits,att_weights
-
-
-
-
-
-
-
-    def training_decoder(self,hn,hend,X,Xindex,Yindex,lens):
+    def training_decoder(self ,hn ,hend ,X ,Xindex ,Yindex ,lens):
         """
 
 
         """
 
-
-        loss_function  = nn.NLLLoss()
-        batch_loss =0
-        LoopN =0
+        loss_function = nn.NLLLoss()
+        batch_loss = 0
+        LoopN = 0
         batch_size = len(lens)
-        for i in range(len(lens)): #Loop batch size
+
+        for i in range(len(lens)):  # Loop batch size
 
             curX_index = Xindex[i]
             curY_index = Yindex[i]
@@ -208,50 +118,33 @@ class PointerNetworks(nn.Module):
             curX = X[i]
 
             x_index_var = Variable(torch.from_numpy(curX_index.astype(np.int64)))
+
             if self.use_cuda:
                 x_index_var = x_index_var.cuda()
 
-            cur_lookup = curX[x_index_var]
+            if self.decoder_type == 'LSTM':  # need h_end,c_end
 
-            # curX_vectors = self.nnEm(cur_lookup)  # output: [seq,features]
-            #
-            # curX_vectors = curX_vectors.unsqueeze(0)  # [batch, seq, features]
-
-
-
-            if self.decoder_type =='LSTM':# need h_end,c_end
-
-
-                h_end = hend[0].permute(1, 0, 2).contiguous().view(batch_size, self.num_rnn_layers,-1)
-                c_end = hend[1].permute(1, 0, 2).contiguous().view(batch_size, self.num_rnn_layers,-1)
+                h_end = hend[0].permute(1, 0, 2).contiguous().view(batch_size, self.num_rnn_layers, -1)
+                c_end = hend[1].permute(1, 0, 2).contiguous().view(batch_size, self.num_rnn_layers, -1)
 
                 curh0 = h_end[i].unsqueeze(0).permute(1, 0, 2)
                 curc0 = c_end[i].unsqueeze(0).permute(1, 0, 2)
 
-
-                h_pass = (curh0,curc0)
-            else:
-                # h_end = hend.permute(1, 0, 2).contiguous().view(batch_size, self.num_rnn_layers,-1)
-                # curh0 = h_end[i].unsqueeze(0).permute(1, 0, 2)
-                # h_pass = curh0
+                h_pass = (curh0, curc0)
+            else: # decoder type GRU
                 h_pass = hend.permute(1, 0, 2)
-                # h_pass = h_pass.squeeze(1)
 
+            decoder_out, _ = self.decoder_rnn(h_pass)  # [seq,features]
 
+            curencoder_hn = hn[i, 0:curL, :]  # hn[batch,seq,H] -->[seq,H] i is loop batch size
 
-            decoder_out,_ = self.decoder_rnn(h_pass)  #[seq,features]
-
-
-            curencoder_hn = hn[i,0:curL,:]  # hn[batch,seq,H] -->[seq,H] i is loop batch size
-
-            for j in range(len(curY_index)):  #Loop di
+            for j in range(len(curY_index)):  # Loop di
                 cur_groundy = curY_index[j]
                 cur_dj = decoder_out[cur_groundy]
                 cur_dj = cur_dj.squeeze(1)
 
-
                 cur_start_index = curX_index[j]
-                predict_range = list(range(cur_start_index,curL))
+                predict_range = list(range(cur_start_index, curL))
 
                 # TODO: make it point backward, only consider predict_range in current time step
                 # align groundtruth
@@ -259,23 +152,18 @@ class PointerNetworks(nn.Module):
                 if self.use_cuda:
                     cur_groundy_var = cur_groundy_var.cuda()
 
-                curencoder_hn_back = curencoder_hn[predict_range,:]
+                curencoder_hn_back = curencoder_hn[predict_range, :]
 
+                cur_logists, cur_weights = self.pointerLayer(curencoder_hn_back, cur_dj)
 
+                batch_loss = batch_loss + loss_function(cur_logists, cur_groundy_var)
+                LoopN = LoopN + 1
 
-
-                cur_logists, cur_weights = self.pointerLayer(curencoder_hn_back,cur_dj)
-
-                batch_loss = batch_loss + loss_function(cur_logists,cur_groundy_var)
-                LoopN = LoopN +1
-
-        batch_loss = batch_loss/LoopN
+        batch_loss = batch_loss / LoopN
 
         return batch_loss
 
-
-    def neg_log_likelihood(self,Xin, Xin_mask, index_decoder_x, index_decoder_y,lens):
-
+    def neg_log_likelihood(self, Xin, Xin_mask, index_decoder_x, index_decoder_y, lens):
         '''
         :param Xin:  stack_x, [allseq,wordDim]
         :param Yin:
@@ -283,31 +171,25 @@ class PointerNetworks(nn.Module):
         :return:
         '''
 
+        encoder_hn, encoder_h_end = self.pointerEncoder(Xin, Xin_mask)
 
-        encoder_hn, encoder_h_end = self.pointerEncoder(Xin, Xin_mask, lens)
-
-        loss = self.training_decoder(encoder_hn, encoder_h_end,Xin,index_decoder_x, index_decoder_y,lens)
+        loss = self.training_decoder(encoder_hn, encoder_h_end, Xin, index_decoder_x, index_decoder_y, lens)
 
         return loss
 
-
-
-
-    def test_decoder(self,hn,hend,X,Yindex,lens):
+    def test_decoder(self, hn, hend, X, Yindex, lens):
 
         loss_function = nn.NLLLoss()
         batch_loss = 0
         LoopN = 0
 
-        batch_boundary =[]
-        batch_boundary_start =[]
-        batch_align_matrix =[]
+        batch_boundary = []
+        batch_boundary_start = []
+        batch_align_matrix = []
 
         batch_size = len(lens)
 
         for i in range(len(lens)):  # Loop batch size
-
-
 
             curL = lens[i]
             curY_index = Yindex[i]
@@ -316,85 +198,69 @@ class PointerNetworks(nn.Module):
                 continue
 
             curX = X[i]
-            cur_end_boundary =curY_index[-1]
+            cur_end_boundary = curY_index[-1]
 
             cur_boundary = []
             cur_b_start = []
             cur_align_matrix = []
-            #
-            # cur_sentence_vectors = self.nnEm(curX)  # output: [seq,features]
 
+            if self.decoder_type == 'LSTM':  # need h_end,c_end
 
-            if self.decoder_type =='LSTM':# need h_end,c_end
-
-
-                h_end = hend[0].permute(1, 0, 2).contiguous().view(batch_size, self.num_rnn_layers,-1)
-                c_end = hend[1].permute(1, 0, 2).contiguous().view(batch_size, self.num_rnn_layers,-1)
+                h_end = hend[0].permute(1, 0, 2).contiguous().view(batch_size, self.num_rnn_layers, -1)
+                c_end = hend[1].permute(1, 0, 2).contiguous().view(batch_size, self.num_rnn_layers, -1)
 
                 curh0 = h_end[i].unsqueeze(0).permute(1, 0, 2)
                 curc0 = c_end[i].unsqueeze(0).permute(1, 0, 2)
 
-                h_pass = (curh0,curc0)
-            else: # only need h_end
+                h_pass = (curh0, curc0)
+            else:  # only need h_end, decoder type GRU
                 h_pass = hend.permute(1, 0, 2)
-
-                # h_end = hend.permute(1, 0, 2).contiguous().view(batch_size, self.num_rnn_layers,-1)
-                # curh0 = h_end[i].unsqueeze(0).permute(1, 0, 2)
-                # h_pass = curh0
-
-
 
             curencoder_hn = hn[i, 0:curL, :]  # hn[batch,seq,H] --> [seq,H]  i is loop batch size
 
             Not_break = True
 
-            # loop_in = cur_sentence_vectors[0,:].unsqueeze(0).unsqueeze(0)  #[1,1,H]
             loop_hc = h_pass
 
+            loopstart = 0
 
-            loopstart =0
-
-            loop_j =0
-            while (Not_break): #if not end
+            loop_j = 0
+            while (Not_break):  # if not end
 
                 loop_o, loop_hc = self.decoder_rnn(h_pass)
                 loop_o = loop_o[loop_j].squeeze(1)
 
+                # TODO: make it point backward
 
-                #TODO: make it point backward
-
-                predict_range = list(range(loopstart,curL))
-                curencoder_hn_back = curencoder_hn[predict_range,:]
+                predict_range = list(range(loopstart, curL))
+                curencoder_hn_back = curencoder_hn[predict_range, :]
                 cur_logists, cur_weights = self.pointerLayer(curencoder_hn_back, loop_o)
 
                 cur_align_vector = np.zeros(curL)
-                cur_align_vector[predict_range]=cur_weights.data.cpu().numpy()[0]
+                cur_align_vector[predict_range] = cur_weights.data.cpu().numpy()[0]
                 cur_align_matrix.append(cur_align_vector)
 
-                #TODO:align groundtruth
-                if loop_j > len(curY_index)-1:
+                # TODO:align groundtruth
+                if loop_j > len(curY_index) - 1:
                     cur_groundy = curY_index[-1]
                 else:
                     cur_groundy = curY_index[loop_j]
 
-
-                cur_groundy_var = Variable(torch.LongTensor([max(0,int(cur_groundy) - loopstart)]))
+                cur_groundy_var = Variable(torch.LongTensor([max(0, int(cur_groundy) - loopstart)]))
                 if self.use_cuda:
                     cur_groundy_var = cur_groundy_var.cuda()
 
                 batch_loss = batch_loss + loss_function(cur_logists, cur_groundy_var)
 
-
-                #TODO: get predicted boundary
+                # TODO: get predicted boundary
                 topv, topi = cur_logists.data.topk(1)
 
                 pred_index = topi[0][0]
 
+                # TODO: align pred_index to original seq
+                ori_pred_index = pred_index + loopstart
 
-                #TODO: align pred_index to original seq
-                ori_pred_index =pred_index + loopstart
-
-                # TODO: changeddddd
+                # TODO: changed part
                 if cur_end_boundary <= ori_pred_index:
                     cur_boundary.append(ori_pred_index)
                     cur_b_start.append(loopstart)
@@ -405,16 +271,14 @@ class PointerNetworks(nn.Module):
                 else:
                     cur_boundary.append(ori_pred_index)
 
-                    # loop_in = cur_sentence_vectors[ori_pred_index+1,:].unsqueeze(0).unsqueeze(0)
                     cur_b_start.append(loopstart)
 
-                    loopstart = ori_pred_index+1  # start =  pred_end + 1
+                    loopstart = ori_pred_index + 1  # start =  pred_end + 1
 
                     loop_j = loop_j + 1
                     LoopN = LoopN + 1
 
-
-            #For each instance in batch
+            # For each instance in batch
             batch_boundary.append(cur_boundary)
             batch_boundary_start.append(cur_b_start)
             batch_align_matrix.append(cur_align_matrix)
@@ -422,32 +286,21 @@ class PointerNetworks(nn.Module):
         if LoopN != 0:
             batch_loss = batch_loss / LoopN
 
-        batch_boundary=np.array(batch_boundary)
+        batch_boundary = np.array(batch_boundary)
         batch_boundary_start = np.array(batch_boundary_start)
         batch_align_matrix = np.array(batch_align_matrix)
 
-        return batch_loss,batch_boundary,batch_boundary_start,batch_align_matrix
+        return batch_loss, batch_boundary, batch_boundary_start, batch_align_matrix
 
+    def predict(self, Xin, Xin_mask, index_decoder_y, lens):
 
+        encoder_hn, encoder_h_end = self.pointerEncoder(Xin, Xin_mask)
 
+        batch_loss, batch_boundary, batch_boundary_start, batch_align_matrix = self.test_decoder(encoder_hn,
+                                                                                                 encoder_h_end, Xin,
+                                                                                                 index_decoder_y, lens)
 
-
-
-
-
-    def predict(self,Xin, Xin_mask, index_decoder_y,lens):
-
-        batch_size = index_decoder_y.shape[0]
-
-        encoder_hn, encoder_h_end = self.pointerEncoder(Xin, Xin_mask, lens)
-
-
-
-
-
-        batch_loss, batch_boundary, batch_boundary_start, batch_align_matrix = self.test_decoder(encoder_hn,encoder_h_end,Xin,index_decoder_y,lens)
-
-        return  batch_loss,batch_boundary,batch_boundary_start,batch_align_matrix
+        return batch_loss, batch_boundary, batch_boundary_start, batch_align_matrix
 
 
 
--- train_segbot_bart.py
+++ train_segbot_bart.py
@@ -17,9 +17,8 @@ test_x_mask = all_masks[idx:]
 test_y = all_boundaries[idx:]
 SAVE_PATH = "C:/Users/qingy/Downloads/FYP/RunSegBot/TrainResults100BART_samelen1"
 
-my_model = PointerNetworks(voca_size=2, voc_embeddings=np.ndarray(shape=(2, 300), dtype=float), word_dim=300,
-                           hidden_dim=10, is_bi_encoder_rnn=True, encoder_type='BART', decoder_type='GRU', rnn_layers=6,
-                           dropout_prob=0.5, use_cuda=False, finedtuning=True, isbanor=True)
+my_model = PointerNetworks(encoder_type='BART', decoder_type='GRU', rnn_layers=6,
+                           dropout_prob=0.5, use_cuda=False)
 
 my_solver = TrainSolver(my_model, train_x=train_x, train_x_mask=train_x_mask, train_y=train_y, dev_x=test_x, dev_x_mask=test_x_mask, dev_y=test_y, save_path=SAVE_PATH,
                         batch_size=1, eval_size=1, epoch=10, lr=1e-2, lr_decay_epoch=1, weight_decay=1e-4,
